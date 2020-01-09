5f3e31313f6cd54c49c6557d7b3a52878cf4a78a
hanneslaner <hannes.laner@akros.ch>
2020-01-09 16:06:48 +0100

    Some fun with fks

--- table.sql
+++ table.sql
@@ -4,11 +4,31 @@ CREATE TABLE Persons
 	LastName  VARCHAR(255),
 	FirstName VARCHAR(255),
 	Address   VARCHAR(255),
-	City      VARCHAR(255)
+	ZipCode   INT
+);
+
+CREATE TABLE PostalCodes
+(
+	ZipCode INT,
+	City    VARCHAR(255),
+	PRIMARY KEY (ZipCode)
 );
 
 INSERT INTO Persons
-	VALUES (1, 'Junior', 'Me', 'Best address', 'MyCity');
+	VALUES (1, 'Junior', 'Me', 'Best address', 3013);
+INSERT INTO Persons
+	VALUES (2, 'Senior', 'Me', 'Best address', 3013);
+INSERT INTO Persons
+	VALUES (3, 'Old', 'You', '2nd Best address', 39100);
+INSERT INTO Persons
+	VALUES (4, 'Young', 'You', '2nd Best address', 39100);
+
+
+INSERT INTO PostalCodes
+	VALUES (3013, 'Bern');
+INSERT INTO PostalCodes
+	VALUES (39100, 'Bozen');
+
 COMMIT;
 
 SELECT *
@@ -29,8 +49,12 @@ DROP INDEX UniquePersonIdIndex;
 ALTER TABLE Persons
 	ADD PRIMARY KEY (PersonId);
 
+ALTER TABLE Persons
+	ADD FOREIGN KEY (ZipCode) REFERENCES PostalCodes (ZipCode);
+
 DROP VIEW ShortPersons;
 DROP TABLE Persons;
+DROP TABLE PostalCodes;
 
 BEGIN
 	dbms_output.put_line('Created, inserted and dropped table Persons');
