14459de7dcd69c4a6b31285afcc090801b736eb5
hanneslaner <hannes.laner@akros.ch>
2020-01-10 12:14:55 +0100

    Procedures are kinda void, functions return stuff

--- procedures.sql
+++ procedures.sql
@@ -5,7 +5,10 @@ BEGIN
 END;
 /
 
-EXECUTE greetings;
+BEGIN
+	greetings();
+END;
+/
 
 --clean up
 DROP PROCEDURE greetings;
@@ -23,11 +26,20 @@ DECLARE
 			z := y;
 		END IF;
 	END;
-
+	
+	FUNCTION findMin(x IN NUMBER, y IN NUMBER) RETURN NUMBER IS
+	BEGIN
+		IF x < y THEN
+			RETURN x;
+		ELSE
+			RETURN y;
+		END IF;
+	END;
 BEGIN
 	a := 22;
 	b := 33;
 	findMin(a, b, c);
 	dbms_output.put_line(' Minimum of (' || a || ', ' || b || ') : ' || c);
+	dbms_output.put_line(' Minimum by function of (' || a || ', ' || b || ') : ' || findMin(a, b));
 END;
 /
